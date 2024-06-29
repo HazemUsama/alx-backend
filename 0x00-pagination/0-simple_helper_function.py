80cdfa6133ca80373763a9f1b7f97f2b7bfd339a
HazemUsama <hazemosama681@gmail.com>
2024-06-29 04:45:46 +0300

    Simple helper

---
+++ 0x00-pagination/0-simple_helper_function.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""
+Simple helper function
+"""
+
+
+def index_range(page, page_size):
+    """return a tuple of size two containing a start index and an end index"""
+    start = (page * page_size) - page_size
+    end = start + page_size
+    return (start, end)
