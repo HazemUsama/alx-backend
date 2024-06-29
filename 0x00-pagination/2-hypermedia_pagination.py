91008b8e7331c01bf13d2914efc1a9dd71a84b4c
HazemUsama <hazemosama681@gmail.com>
2024-06-29 05:26:02 +0300

    Hypermedia pagination

---
+++ 0x00-pagination/2-hypermedia_pagination.py
@@ -0,0 +1,60 @@
+#!/usr/bin/env python3
+"""Hypermedia pagination """
+import csv
+import math
+from typing import List, Tuple, Dict
+
+
+class Server:
+    """Server class to paginate a database of popular baby names.
+    """
+    DATA_FILE = "Popular_Baby_Names.csv"
+
+    def __init__(self):
+        self.__dataset = None
+
+    def dataset(self) -> List[List]:
+        """Cached dataset
+        """
+        if self.__dataset is None:
+            with open(self.DATA_FILE) as f:
+                reader = csv.reader(f)
+                dataset = [row for row in reader]
+            self.__dataset = dataset[1:]
+
+        return self.__dataset
+
+    @staticmethod
+    def index_range(page: int, page_size: int) -> Tuple[int, int]:
+        """
+        return a tuple of size two containing a start index and an end index
+        """
+        start = (page * page_size) - page_size
+        end = start + page_size
+        return (start, end)
+
+    def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
+        """Get Page"""
+        assert type(page_size) is int and type(page) is int
+        assert page > 0 and page_size > 0
+        self.dataset()
+        start, end = self.index_range(page, page_size)
+        return self.__dataset[start:end]
+
+    def get_hyper(page: int = 1, page_size: int = 10) -> Dict:
+        """
+        return A dictionary of the following:
+        page_size, page, data, next_page, prev_page, total_pages
+        """
+        data = self.get_page(page, page_size)
+        data_len = len(self.dataset())
+        total_pages = int(date_len / page_size)
+
+        return {
+            "page_size": len(data),
+            "page": page,
+            "data": data,
+            "next_page": page + 1 if page + 1 < total_pages else None,
+            "prev_page": page - 1 if page - 1 > 0 else None,
+            "total_pages": total_pages
+        }
