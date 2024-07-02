50e697aed277c20a37939c9785709d64cb89b45a
HazemUsama <hazemosama681@gmail.com>
2024-07-03 02:11:00 +0300

    type-annotated

--- 0x02-i18n/1-app.py
+++ 0x02-i18n/1-app.py
@@ -1,9 +1,19 @@
 #!/usr/bin/env python3
 """Simple Flask App module"""
 from flask import Flask, render_template
+from flask_babel import Babel
+
+
+class Config:
+    """Config class for Babel"""
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = "en"
+    BABEL_DEFAULT_TIMEZONE = "UTC"
 
 
 app = Flask(__name__)
+babel = Babel(app)
+app.config.from_object(Config)
 
 
 @app.route('/', strict_slashes=False)
