965d237bd74c7caf674302c32dacd81ae2b85bd7
HazemUsama <hazemosama681@gmail.com>
2024-07-05 22:45:27 +0300

    Mock logging in

--- 0x02-i18n/4-app.py
+++ 0x02-i18n/4-app.py
@@ -28,7 +28,7 @@ def get_locale() -> str:
 @app.route('/', strict_slashes=False)
 def home() -> str:
     """renders a simple html file"""
-    return render_template('3-index.html')
+    return render_template('4-index.html')
 
 
 if __name__ == '__main__':
